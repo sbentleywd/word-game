70238c37f79173a366d0ac3632f86ae2afe56db3
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2022-02-19 17:03:49 +0000

    init blank app

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-main>
+      <Game/>
+    </v-main>
+  </v-app>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
+import Game from './components/Game.vue'
 
 export default Vue.extend({
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    Game
+  },
+
+  data: () => ({
+    //
+  })
 })
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
