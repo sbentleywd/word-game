9647392d8d3ec99addd1aea48481261e6c3c5f50
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2022-02-20 14:05:43 +0000

    Add inital store values

--- src/store/index.ts
+++ src/store/index.ts
@@ -5,8 +5,15 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    answer: 'steam',
+    guesses: [['a', 'b', 'c', 'd', 'e']],
+    correct: [],
+    wrongPlace: []
   },
   mutations: {
+    setState (state: any, payload: { property: string, value: any }) {
+      state[payload.property] = payload.value
+    }
   },
   actions: {
   },
