2efe570b210396e81110836f6958402ee527c036
Simon Bentley <61815607+sbentleywd@users.noreply.github.com>
2022-02-22 17:39:02 +0000

    Color guessed letters

--- src/components/Guess.vue
+++ src/components/Guess.vue
@@ -1,6 +1,15 @@
 <template>
   <div>
-    <v-sheet v-for="(letter, letterIndex) in this.$props.guess" :key="letterIndex" outlined height="50" width="50" class="mx-2 d-flex justify-center align-center"><span>{{ letter }}</span></v-sheet>
+    <v-sheet
+      v-for="(letter, letterIndex) in this.$props.guess"
+      :key="letterIndex"
+      outlined height="50"
+      width="50"
+      :color="mappedGuess.find(obj => obj.index === letterIndex).rightPlace ? 'green lighten-3' : yellow[letterIndex] ? 'yellow lighten-3' : 'grey lighten-3'"
+      class="mx-2 d-flex justify-center align-center"
+    >
+        <span>{{ letter }}</span>
+      </v-sheet>
   </div>
 </template>
 
@@ -26,6 +35,18 @@ export default Vue.extend({
           letter,
           index,
           rightPlace: this.answer[index] === letter
+        }
+      })
+    },
+    yellow (): boolean[] {
+      return this.$props.guess.map((letter: string, guessIndex: number) => {
+        const answerNo = this.answerCount[letter]
+        const correctCount = this.mappedGuess.filter((obj: { letter: string, index: number, rightPlace: boolean }) => obj.letter === letter && obj.rightPlace).length
+        const alreadyChecked = this.$props.guess.slice(0, guessIndex).filter((item: string) => item === letter).length
+        if (!answerNo || correctCount === answerNo || answerNo - correctCount - alreadyChecked < 1) {
+          return false
+        } else {
+          return true
         }
       })
     }
